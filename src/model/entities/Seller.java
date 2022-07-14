d9e5e875ff8f0c6b96024166a523ecbf100fa915
papait <leonardovareschi@gmail.com>
2022-07-14 20:23:13 -0300

    InterfacesDAO

--- src/model/entities/Seller.java
+++ src/model/entities/Seller.java
@@ -1,5 +1,106 @@
 package model.entities;
 
-public class Seller {
+import java.io.Serializable;
+import java.util.Date;
+import java.util.Objects;
 
+public class Seller implements Serializable {
+
+
+	private static final long serialVersionUID = 1L;
+	
+	private Integer id;
+	private String name;
+	private String email;
+	private Date birthDate;
+	private Double baseSalaru;
+	
+	private Department department;
+	
+	public Seller () {
+		
+	}
+
+	public Seller(Integer id, String name, String email, Date birthDate, Double baseSalaru, Department department) {
+		this.id = id;
+		this.name = name;
+		this.email = email;
+		this.birthDate = birthDate;
+		this.baseSalaru = baseSalaru;
+		this.department = department;
+	}
+	
+	public Integer getId() {
+		return id;
+	}
+	
+	public void setId (Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Date getBirthDate() {
+		return birthDate;
+	}
+
+	public void setBirthDate(Date birthDate) {
+		this.birthDate = birthDate;
+	}
+
+	public Double getBaseSalaru() {
+		return baseSalaru;
+	}
+
+	public void setBaseSalaru(Double baseSalaru) {
+		this.baseSalaru = baseSalaru;
+	}
+
+	public Department getDepartment() {
+		return department;
+	}
+
+	public void setDepartment(Department department) {
+		this.department = department;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Seller other = (Seller) obj;
+		return Objects.equals(id, other.id);
+	}
+
+	@Override
+	public String toString() {
+		return "Seller [id=" + id + ", name=" + name + ", email=" + email + ", birthDate=" + birthDate + ", baseSalaru="
+				+ baseSalaru + ", department=" + department + "]";
+	}
+	
+	
+	
 }
